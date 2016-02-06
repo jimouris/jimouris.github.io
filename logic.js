8d7ab66cce689aee0eb521d4a3bbd5fb6a32b10f
gpelelis <gpelelis@gmail.com>
2016-02-06 20:29:56 +0200

    refactor, statefull about-contact & meta
    refactor html, css, js. Added meta tags and facebook share image.
    Contact stays on reload

---
+++ logic.js
@@ -0,0 +1,32 @@
+// move from one section to another
+var toggleSections = function(){
+	$('.about-me').toggleClass('move-n-hide');
+    $(this).removeClass('inactive').addClass("active");
+    $(this).siblings().addClass('inactive').removeClass('active');
+};
+
+// get active section on reload
+var section = location.hash;
+if (section == '#contact'){
+	toggleSections();
+	$('#contact').removeClass('inactive').addClass("active");
+    $('#about').addClass('inactive').removeClass('active');
+}
+
+
+// once everything is setup then display content
+$('.all-wrap').css('opacity', '1');
+
+// functionality for navigation items
+$('nav').delegate('.inactive', 'click', toggleSections);
+
+
+// use for pagination
+$('#contact').click(function(){
+	location.hash = 'contact';
+});
+
+
+$('#about').click(function(){
+	location.hash = 'about';
+});
